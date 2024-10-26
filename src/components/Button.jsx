ae1419aaca076a2dcf253b2f319e63dcefceb63a
TCB2307 <buituancuong44@gmail.com>
2024-10-26 18:56:16 +1100

    second blood

---
+++ src/components/Button.jsx
@@ -0,0 +1,7 @@
+export default function Button({ text }) {
+  return (
+    <div className="text-xs bg-[#14BECA] text-white rounded-3xl px-4 py-2">
+      {text}
+    </div>
+  );
+}
--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,10 +1,37 @@
 import "../css/main.css";
+import myAva from "../images/avatar.png";
+import Button from "../components/Button";
 
 function Homepage() {
   return (
-    <div className="text-3xl font-bold underline">
-      <p>Hello</p>
-      <p>Hello</p>
+    <div className="px-12 py-8">
+      <div className="flex py-8 space-x-14">
+        <div className="flex flex-col space-y-4">
+          <div className="text-6xl font-bold py-4">I'm Andy</div>
+          <div className="flex space-x-3 font-medium text-3xl">
+            <div className="px-4 py-2 border-2 text-[#14BECA]">Digital</div>
+            <div className="flex items-center">&</div>
+            <div className="px-4 py-2 border-2 text-[#14BECA]">
+              Frontend Developer
+            </div>
+          </div>
+          <div className="text-1xl">
+            I design and build clean, simple solutions websites with code.
+            Bringing ideas to life through thoughtful design is what I love
+            most.
+          </div>
+          <div className="flex space-x-4 text-1xl">
+            <Button text="WEBSITES" />
+            <Button text="EDMS" />
+            <Button text="ANIMATIONS" />
+          </div>
+          <div className="text-1xl">FIND OUT MORE</div>
+          <div className="text-1xl">BUTTON</div>
+        </div>
+        <div className="text-1xl">
+          <img src={myAva} alt="Avatar" />
+        </div>
+      </div>
     </div>
   );
 }
