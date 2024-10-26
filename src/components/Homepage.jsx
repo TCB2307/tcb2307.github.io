b15688fbff78b16d6a610e5b2b1b01ec1b62e5bc
TCB2307 <buituancuong44@gmail.com>
2024-10-26 17:37:02 +1100

    added tailwind

---
+++ src/components/Homepage.jsx
@@ -0,0 +1,12 @@
+import "../css/main.css";
+
+function Homepage() {
+  return (
+    <div className="text-3xl font-bold underline">
+      <p>Hello</p>
+      <p>Hello</p>
+    </div>
+  );
+}
+
+export default Homepage;
