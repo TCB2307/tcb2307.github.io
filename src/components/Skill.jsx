bd1016d05ca7de381bba64cde57e48f5e3ed8f51
TCB2307 <buituancuong44@gmail.com>
2024-11-10 18:46:05 +1100

    fix bug and manifest code

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -1,3 +1,4 @@
+import SkillDetail from "./SkillDetail";
 import website from "../images/website.png";
 import email from "../images/email-icon.png";
 import banner from "../images/banner.png";
@@ -12,53 +13,7 @@ import gulp from "../images/gulp.png";
 import gsap from "../images/gsap.png";
 import figma from "../images/figma.png";
 import photoshop from "../images/photoshop.png";
-
-function SkillDetail({
-  img,
-  rightSideBorder,
-  bottomSideBorder,
-  title,
-  description,
-  tools,
-}) {
-  const borderCss = rightSideBorder === true ? "border-r-2" : "";
-  const borderCssSm = bottomSideBorder === true ? "border-b-2" : "";
-
-  return (
-    <div
-      className={`${borderCss} border-black border-opacity-10 w-full h-[36rem] skill-m:border-r-0 skill-m:${borderCssSm} skill-m:h-[30rem]`}
-    >
-      <div className="flex justify-center mt-16">
-        <img src={img} alt="logo" className="w-20" />
-      </div>
-      <div className=" flex justify-center mt-8">
-        <div className="text-center w-4/5 about:w-[85%] skill-text:w-[90%] skill-sm:w-[80%]">
-          <div className="text-2xl font-semibold about:text-[1.3rem] skill-text:text-[1.2rem] skill-m:text-2xl">
-            {title}
-          </div>
-          <div className="mt-6 font-normal h-[9rem] about:text-[0.95rem] skill-sm:h-[6rem]">
-            {description}
-          </div>
-          <div className="text-2xl mt-6 text-[#EB4A4A] font-medium about:mt-14 skill-text:mt-20 skill-sm:mt-14">
-            Tools
-          </div>
-          <div className="font-light mb-10 mt-4">
-            <div className="flex space-x-2 justify-center">
-              {tools.map((tool, index) => (
-                <img
-                  key={index}
-                  src={tool}
-                  alt="logo"
-                  className="w-14 about:w-[3rem] skill-text:w-[2.7rem] skill-sm:w-[3.5rem]"
-                />
-              ))}
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-}
+import { useEffect } from "react";
 
 export default function Skill() {
   return (
@@ -68,27 +23,27 @@ export default function Skill() {
           <SkillDetail
             img={website}
             rightSideBorder={true}
-            bottomSideBorder={true}
             title="Website"
             description="I create responsive, user-centered websites that are visually appealing and performance-optimized, leveraging the latest front-end technologies to deliver smooth, intuitive designs across all devices."
             tools={[html, tailwind, js, react, typescript]}
+            skill="skill-m:border-b-2"
           />
           <SkillDetail
             img={email}
             rightSideBorder={true}
-            bottomSideBorder={true}
             title="Electrical Direct Emails"
             description="
 Designed dynamic EDMs for clients like Myer and NAB, creating visually impactful advertising campaigns that effectively conveyed the brand's message and drove audience engagement across digital platforms."
             tools={[html, css, salesforce, gulp, figma]}
+            skill="skill-m:border-b-2"
           />
           <SkillDetail
             img={banner}
             rightSideBorder={false}
-            bottomSideBorder={false}
             title="Banner"
             description="Developed engaging banners using GSAP, in order to create smooth animations to highlight brand messages. Focused on high-performance designs for a great user experience across all devices."
             tools={[html, css, js, gsap, photoshop]}
+            skill=""
           />
         </div>
       </div>
---
+++ src/components/SkillDetail.jsx
@@ -0,0 +1,47 @@
+function SkillDetail({
+  img,
+  rightSideBorder,
+  title,
+  description,
+  tools,
+  skill,
+}) {
+  const borderCss = rightSideBorder === true ? "border-r-2" : "";
+
+  return (
+    <div
+      className={`${borderCss} border-black border-opacity-10 w-full h-[36rem] skill-m:border-r-0 skill-m:h-[30rem] ${skill}`}
+    >
+      <div className="flex justify-center mt-16">
+        <img src={img} alt="logo" className="w-20" />
+      </div>
+      <div className=" flex justify-center mt-8">
+        <div className="text-center w-4/5 about:w-[85%] skill-text:w-[90%] skill-sm:w-[80%]">
+          <div className="text-2xl font-semibold about:text-[1.3rem] skill-text:text-[1.2rem] skill-m:text-2xl">
+            {title}
+          </div>
+          <div className="mt-6 font-normal h-[9rem] about:text-[0.95rem] skill-sm:h-[6rem]">
+            {description}
+          </div>
+          <div className="text-2xl mt-6 text-[#EB4A4A] font-medium about:mt-14 skill-text:mt-20 skill-sm:mt-14">
+            Tools
+          </div>
+          <div className="font-light mb-10 mt-4">
+            <div className="flex space-x-2 justify-center">
+              {tools.map((tool, index) => (
+                <img
+                  key={index}
+                  src={tool}
+                  alt="logo"
+                  className="w-14 about:w-[3rem] skill-text:w-[2.7rem] skill-sm:w-[3.5rem]"
+                />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SkillDetail;
