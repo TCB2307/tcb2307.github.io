a84d0706e40c73ee6f8315cbdb2e1fffd7f23433
TCB2307 <buituancuong44@gmail.com>
2024-11-10 19:10:59 +1100

    clean code

--- src/components/SkillDetail.jsx
+++ src/components/SkillDetail.jsx
@@ -16,14 +16,14 @@ function SkillDetail({
         <img src={img} alt="logo" className="w-20" />
       </div>
       <div className=" flex justify-center mt-8">
-        <div className="text-center w-4/5 about:w-[85%] skill-text:w-[90%] skill-sm:w-[80%]">
+        <div className="text-center w-4/5 about:w-[85%] skill-text:w-[90%] skill-sm:w-[80%] phone:w-[90%]">
           <div className="text-2xl font-semibold about:text-[1.3rem] skill-text:text-[1.2rem] skill-m:text-2xl">
             {title}
           </div>
           <div className="mt-6 font-normal h-[9rem] about:text-[0.95rem] skill-sm:h-[6rem]">
             {description}
           </div>
-          <div className="text-2xl mt-6 text-[#EB4A4A] font-medium about:mt-14 skill-text:mt-20 skill-sm:mt-14">
+          <div className="text-2xl mt-6 text-[#EB4A4A] font-medium about:mt-14 skill-text:mt-20 skill-sm:mt-14 phone:mt-8">
             Tools
           </div>
           <div className="font-light mb-10 mt-4">
@@ -33,7 +33,7 @@ function SkillDetail({
                   key={index}
                   src={tool}
                   alt="logo"
-                  className="w-14 about:w-[3rem] skill-text:w-[2.7rem] skill-sm:w-[3.5rem]"
+                  className="w-14 about:w-[3rem] skill-text:w-[2.7rem] skill-sm:w-[3.5rem] phone:w-[3.1rem]"
                 />
               ))}
             </div>
